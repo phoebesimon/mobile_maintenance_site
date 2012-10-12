64432a143548384765b7b998aaade4bb531a8c26
Phoebe Simon <phoebesimon@hotmail.com>
2012-10-12 14:56:54 -0700

    Initial commit with blank Ruby app

---
+++ fixit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fixit::Application.config.secret_token = '5d5ab59289dddc64024dd2b70756800dbab27187f8e408cadd611fef4deae9b16828e4ccf4fcf4e5a0518d6b050ae1ae6eb8b24d353656369e4f4ddae0737e23'
